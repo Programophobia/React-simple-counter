b12de6915754cb1c7004fae936305d91892bcf8f
Anna Powaska <annapowaska@op.pl>
2024-01-26 10:11:42 +0100

    Add start/stop/reset function and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import Button from "./components/Button/Button";
 import TimeCounter from "./components/TimeCounter/TimeCounter";
-
+import styles from './App.module.scss'
 
 const App = () => {
+  const [time, setTime]= useState(0);
+  const [timer, setTimer] = useState();
+
+  const startStoper = () =>{
+    setTimer(setInterval(() =>
+    { setTime(prevValue => prevValue + 10);}))
+  };
+
+  const stopStoper = () =>{
+    clearInterval(timer);}
+
+  const resetStoper = () => {
+    stopStoper();
+    setTime(0);
+    }
+
   return (
-    <section>
-       <TimeCounter />
-       <Button>Start</Button>
-       <Button>Stop</Button>
-       <Button>Reset</Button>
-    </section>
-   
+    <div className={styles.container}>
+      <TimeCounter time={time}/>
+      <Button action={startStoper}>START</Button>
+      <Button action={stopStoper}>STOP</Button>
+      <Button action={resetStoper}>RESET</Button>
+    </div>
   );
-};
+  }
 
 export default App;
--- src/components/TimeCounter/TimeCounter.js
+++ src/components/TimeCounter/TimeCounter.js
@@ -1,17 +1,19 @@
 
-    const TimeCounter = props => {
+   import styles from './TimeCounter.module.scss'
+   
+   
+   const TimeCounter = props => {
 
         const time = ms => { 
-          let sec = Math.floor((ms / 1000) % 60);
-          let min = Math.floor((ms / (1000 * 60)) % 60);
-          let hour = Math.floor((ms / (1000 * 60 * 60)) % 24)
-          let msec = Math.floor((ms % 1000) / 100);
-          return (`${hour} : ${min} : ${sec} : ${msec}`);
-        }
-      
+            let sec = Math.floor((ms / 1000 ) % 60);
+            let min = Math.floor(((ms / 1000 ) / 60) % 60);
+            let hour = Math.floor((ms / 1000 ) / 60 / 60);
+            let msec = Math.floor((ms % 1000) / 100 );
+            return (`${hour} : ${min} : ${sec} : ${msec}`);
+          }
         return (
-          <div>
-            {time(props.miliseconds)}
+          <div className={styles.time}>
+            {time(props.time)}
           </div>
         );
       }
